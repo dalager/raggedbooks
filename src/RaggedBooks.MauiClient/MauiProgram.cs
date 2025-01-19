6619a05deb7db63b78afc4177e9f819162c1c41c
Christian Dalager <christian@dalager.com>
2025-01-19 19:38:21 +0100

    Blank client running but with dependencies injected

---
+++ src/RaggedBooks.MauiClient/MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace RaggedBooks.MauiClient
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("Appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+            var builder = MauiApp.CreateBuilder();
+            builder.Configuration.AddConfiguration(configuration);
+
+            builder
+                .UseMauiApp<App>()
+                .RegisterAppServices()
+                .RegisterViewModels()
+                .RegisterViews()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
